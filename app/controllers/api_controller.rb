fd81d3f9bc5a11bb59d429cd8eab6c4d170d3801
Bartosz Markowski <nenros@gmail.com>
2015-05-20 13:13:23 +0200

    Remove ember-cli app

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -4,6 +4,24 @@ class ApiController < ApplicationController
   before_action :set_cors_headers
   after_action :set_cors_headers
 
+  rescue_from(ActionController::ParameterMissing) do |parameter_missing_exception|
+    error = {}
+    error[parameter_missing_exception.param] = ['parameter is required']
+    response = { errors: [error] }
+    respond_to do |format|
+      format.json { render json: response, status: :unprocessable_entity }
+    end
+  end
+
+  rescue_from(ActionController::UnpermittedParameters) do |parameter_missing_exception|
+    error = {}
+    error[parameter_missing_exception.params] = ['parameter is required']
+    response = { errors: [error] }
+    respond_to do |format|
+      format.json { render json: response, status: :unprocessable_entity }
+    end
+  end
+
   def set_cors_headers
     headers['Access-Control-Allow-Origin'] = '*'
     headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
