4031980d2b764f1290833374ba34786b04c0ad8f
Bartosz Markowski <nenros@gmail.com>
2015-09-02 10:24:03 +0200

    remove rails

--- db/migrate/20150424220248_add_column_trello_url_to_walk.rb
+++
@@ -1,9 +0,0 @@
-class AddColumnTrelloUrlToWalk < ActiveRecord::Migration
-  def change
-    add_column(:walks, 'trello_url', :string)
-  end
-
-  def down
-    remove_column(:walks, 'trello_url')
-  end
-end
